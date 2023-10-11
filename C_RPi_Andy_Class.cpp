573d9c8d49045926859d6c2f647e40738498a61f
ragcans <102828595+ragcans@users.noreply.github.com>
2023-10-11 13:58:57 -0400

    seperated the SonicSole class into its own file/headers

---
+++ C_RPi_Andy_Class.cpp
@@ -0,0 +1,154 @@
+#include "SonicSole.h"
+
+uint64_t getMicrosTimeStamp() {
+    struct timeval tv;
+	gettimeofday(&tv,NULL);
+	return tv.tv_sec*(uint64_t)1000000+tv.tv_usec;
+}
+
+void UDPSend(int sockfd, const int *reading, socklen_t len, struct sockaddr_in servaddr) {
+    sendto(sockfd, (const int *)reading, len,
+           MSG_CONFIRM, (const struct sockaddr *) &servaddr,
+           sizeof(servaddr));
+}
+
+SonicSole::SonicSole() {
+    startTime = getMicrosTimeStamp();
+    wiringPiSetupGpio() ;
+    pinMode(CS, OUTPUT) ;
+    digitalWrite(CS,HIGH);
+    cout << "Initializing SPI" << endl ;
+
+    // INITIALIZING UART1
+    printf("Initializing UART0...\n\n");
+
+    // INITIALIZING GPIO (Use wPi pins, not BCM)
+    printf("Initializing GPIO...\n\n");
+    if (wiringPiSetupGpio () == -1)
+    {
+        fprintf (stdout, "Unable to start wiringPi: %s\n", strerror (errno)) ;
+    }
+    printf("GPIO initialized successfully!\n\n");
+
+
+    int fd = wiringPiSPISetupMode(SPI_CHANNEL, 1000000, 0);
+    if (fd == -1)
+    {
+        std::cout << "Failed to init SPI communication.\n";
+    }
+    std::cout << "SPI communication successfully setup.\n";
+
+    this_thread::sleep_for(chrono::milliseconds(500));
+}
+
+
+void SonicSole::motorVibrate() {
+    digitalWrite(23, HIGH); //Turn motors on and off to show device is on
+    digitalWrite(20, HIGH);
+    delay(1000);
+    digitalWrite(23, LOW);
+    digitalWrite(20, LOW);
+}
+
+void SonicSole::detectModeChange() {
+    if (detectHeelThreshold() && heelThresholdInterval < 1)
+        mode = !mode;
+        string text = mode ? "Switched to Sound Mode" : "Switched to Vibration Mode";
+}
+
+void SonicSole::runVibrateMode() {
+    if (detectCombinedThreshold()) {
+        playSound();
+    }
+}
+
+void SonicSole::toCSV() {
+    return;
+}
+
+uint64_t SonicSole::getRunningTime() {
+    return currentTime - startTime;
+}
+
+void SonicSole::updateCurrentTime() {
+    currentTime = getCurrentTime();
+}
+
+void SonicSole::updatePressure() {
+    prevHeelPressure = currHeelPressure;
+    prevForePressure =  currForePressure;
+    prevCombinedPressure = currCombinedPressure;
+    
+    currHeelPressure = getSensorReadings(160);
+    currForePressure = getSensorReadings(224);
+    currCombinedPressure = currHeelPressure + currForePressure;
+
+    if (currCombinedPressure < minCombinedPressure)
+        minCombinedPressure = currCombinedPressure;
+
+    if (currCombinedPressure > maxCombinedPressure)
+        maxCombinedPressure = currCombinedPressure;
+
+    if (currHeelPressure < minHeelPressure)
+        minHeelPressure = currHeelPressure;
+
+    if (currHeelPressure > maxHeelPressure)
+        maxHeelPressure = currHeelPressure;
+
+    cout << "currHeelPressure: " << currHeelPressure << endl;
+    cout << "currForePressure: " << currForePressure << endl;
+
+}
+
+int SonicSole::getSensorReadings(unsigned char signal) {
+    // ADC channels
+    digitalWrite(CS,LOW);
+    SPIbuff[0] = 1;
+    SPIbuff[1] = signal;
+    SPIbuff[2] = 0;
+    wiringPiSPIDataRW(SPI_CHANNEL,SPIbuff,3);
+    int sensorReading = SPIbuff[1] << 8 | SPIbuff[2];
+    digitalWrite(CS, HIGH);
+    return sensorReading;
+}
+
+bool SonicSole::getMode() {
+    return mode;
+}
+
+uint64_t SonicSole::getCurrentTime() {
+    return getMicrosTimeStamp();
+}
+
+void SonicSole::updateHeelThresholdInterval() {
+    previousHeelThresholdTime = currentHeelThresholdTime;
+    currentHeelThresholdTime = getCurrentTime();
+    heelThresholdInterval = currentHeelThresholdTime - previousHeelThresholdTime;    
+}
+
+bool SonicSole::detectThreshold(int prevReading, int currReading, int minReading, int maxReading) {
+    double threshold = 0.1 * (maxReading - minReading) + minReading;
+    if (prevReading < threshold && currReading > threshold) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+bool SonicSole::detectHeelThreshold() {
+    bool thresholdDetected = detectThreshold(prevHeelPressure, currHeelPressure, minHeelPressure, maxHeelPressure);
+    if (thresholdDetected)
+        updateHeelThresholdInterval();
+    return thresholdDetected;
+}
+
+bool SonicSole::detectCombinedThreshold() {
+    return detectThreshold(prevCombinedPressure, currCombinedPressure, minCombinedPressure, maxCombinedPressure);
+}
+
+void SonicSole::playSound() {
+    cout << "Played Sound" << endl;
+}
+
+
+ 
---
+++ C_RPi_Andy_Class.h
@@ -0,0 +1,107 @@
+// NPi Sensor Read and Data Logging
+
+#include <iostream>
+#include <fstream>
+#include <mutex>
+#include <thread>
+#include <condition_variable>
+#include <chrono>
+#include <iomanip>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+#include <inttypes.h>
+#include <sys/time.h>
+#include <sys/ioctl.h>
+#include <linux/i2c-dev.h>
+#include <time.h>
+
+// WiringPi libraries
+#include <wiringPi.h>
+#include <wiringSerial.h>
+#include <wiringPiI2C.h>
+#include <termios.h>
+// #include "wiringSerial.h"
+
+// User-defined libraries
+#include "RPi_combined_Header.h"
+#include "RPi_Raj_Header.hpp"
+
+// libraries from ADC code
+#include <wiringPiSPI.h>
+
+// libraries for UDP
+#include <bits/stdc++.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+#define MAX_RUN_TIME 600000000
+#pragma once  
+
+using namespace std;
+
+static int SPI_CHANNEL = 0;
+static int CS = 17;
+
+mutex dataMutex[3];
+mutex writeMutex;
+condition_variable condWrite;
+
+uint64_t getMicrosTimeStamp();
+
+void UDPSend(int sockfd, const int *reading, socklen_t len, struct sockaddr_in servaddr);
+
+class SonicSole {
+public:
+    bool mode = true;
+    double maxHeelPressure = 0;
+    double minHeelPressure = 1000;
+
+    double maxCombinedPressure = 0;
+    double minCombinedPressure = 1000;
+
+    double currHeelPressure = 0;
+    double prevHeelPressure = 0;
+
+    double currForePressure = 0;
+    double prevForePressure = 0;
+
+    double currCombinedPressure = 0;
+    double prevCombinedPressure = 0;
+
+    uint64_t startTime = 0;
+    uint64_t currentTime = 0;
+
+    SonicSole();
+    void motorVibrate();
+    void detectModeChange();
+    void runSoundMode();
+    void runVibrateMode();
+    void toCSV();
+    uint64_t getRunningTime();
+    void updateCurrentTime();
+    void updatePressure();
+    int getSensorReadings(unsigned char signal);
+    bool getMode();
+
+private: 
+    double heelThresholdInterval = 0;
+    double previousHeelThresholdTime = 0;
+    double currentHeelThresholdTime = 0;
+    unsigned char SPIbuff[3];
+
+    void updateHeelThresholdInterval();
+    bool detectThreshold(int prevReading, int currReading, int minReading, int maxReading);
+    bool detectHeelThreshold();
+    bool detectCombinedThreshold();
+    void playSound();
+    uint64_t getCurrentTime();
+};
