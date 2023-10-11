766c717760fca185c5db4b5e341490845f64fd58
ragcans <102828595+ragcans@users.noreply.github.com>
2023-10-11 10:09:40 -0400

    test

--- C_RPi_no_IMU.cpp
+++ C_RPi_no_IMU.cpp
@@ -113,6 +113,7 @@ int main(int argc, char *argv[])
   int threshold_pressure_sum;
   double threshold_time_array[20] = {};
   double start_time = getMicrosTimeStamp();
+  cout << "start_time:" << start_time << endl;
   double current_threshold_time = 0;
 
   int max_pressure = 1000;
