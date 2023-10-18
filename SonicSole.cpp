cbe6d7a3aa4bf14bc5b19abb891ecb9f377bd9d8
ragcans <102828595+ragcans@users.noreply.github.com>
2023-10-18 11:15:42 -0400

    bug fix

--- SonicSole.cpp
+++ SonicSole.cpp
@@ -42,10 +42,11 @@ SonicSole::SonicSole() {
 }
 
 void SonicSole::detectModeChange() {
-    if (thresholdCross == 3) //&& detectHeelThreshold() && heelThresholdInterval < 1)
+    if (thresholdCross == 3) {//&& detectHeelThreshold() && heelThresholdInterval < 1)
         mode = !mode;
         thresholdCross = 0;
         string text = mode ? "Switched to Sound Mode" : "Switched to Vibration Mode";
+    }
 }
 
 void SonicSole::toCSV() {
