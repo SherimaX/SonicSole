fc39e7d0bb7bb98ebf22fbc9d26f675e9e17ccae
ragcans <102828595+ragcans@users.noreply.github.com>
2023-10-25 12:10:54 -0400

    added IMU method to class file

--- SonicSole.h
+++ SonicSole.h
@@ -93,6 +93,7 @@ public:
     void runSoundMode();
     void runVibrateMode();
     void toCSV();
+    void IMU();
     uint64_t getRunningTime();
     void updateCurrentTime();
     void updatePressure();
