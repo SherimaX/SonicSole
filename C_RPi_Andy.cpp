36756513d388dde3e885115bb35688d17b399304
ragcans <102828595+ragcans@users.noreply.github.com>
2023-11-15 14:10:08 -0500

    added delay in main function

--- C_RPi_Andy.cpp
+++ C_RPi_Andy.cpp
@@ -35,7 +35,7 @@ int main(int argc, char* argv[])
       
       sole->toCSV();
       cout << "\n";
-      // delay(100);
+      delay(100);
       // if (sole->getRunningTime() > MAX_RUN_TIME) {
       //    cout << sole->getRunningTime() << endl;
       //    cout << MAX_RUN_TIME << endl;
