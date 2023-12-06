eef7d0d82a975556b94133d9a36b9cfc405e8026
ragcans <102828595+ragcans@users.noreply.github.com>
2023-12-06 15:09:41 -0500

    added test file to test if UDPSending works

---
+++ UDP_Test.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <cstring>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+
+#define PORT 25000
+
+void sendFlexSensorData(int flexSensorData) {
+    int sockfd;
+    struct sockaddr_in serverAddr;
+
+    // UDP Socket
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
+        std::cerr << "Error creating socket" << std::endl;
+        return;
+    }
+
+    memset(&serverAddr, 0, sizeof(serverAddr));
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(PORT);
+    serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1"); // localhost
+
+    /*
+    if (sendto(sockfd, &flexData, sizeof(flexData), 0, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) == -1) {
+        std::cerr << "Error sending data" << std::endl;
+    } else {
+        std::cout << "Flex sensor data sent successfully!" << std::endl;
+    }
+    */
+
+    try {
+        UDPSend(sockfd, &flexSensorData, sizeof(flexSensorData), serverAddr);
+        // sendto(sockfd, &flexData, sizeof(flexData), 0, (struct sockaddr *)&serverAddr, sizeof(serverAddr));
+    }
+    catch (...) {
+        std:cout << "Error: UDPSend cannot send data" << endl;
+    }
+
+    std::cout << "Data sent to UDP" << endl;
+    close(sockfd);
+}
+
+void UDPSend(int sockfd, const int *reading, socklen_t len, struct sockaddr_in servaddr) {
+    sendto(sockfd, (const int *)reading, len,
+           MSG_CONFIRM, (const struct sockaddr *) &servaddr,
+           sizeof(servaddr));
+}
