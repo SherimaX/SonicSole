966544e8b9eeeae051c7b6808020ae5178881363
ragcans <102828595+ragcans@users.noreply.github.com>
2024-01-03 13:22:12 -0500

    testing

--- SonicSole.cpp
+++ SonicSole.cpp
@@ -93,7 +93,7 @@ SonicSole::SonicSole() {
 void SonicSole::detectModeChange() {
     startInterval = getSecondsTimeStamp();
     detectHeelThreshold();    
-    if (thresholdCross == 3 && heelThresholdInterval < 3 && (endInterval - startInterval < 1)) {
+    if (thresholdCross == 3 && heelThresholdInterval < 3) { //&& (endInterval - startInterval < 1)) {
         mode = !mode;
         thresholdCross = 0;
         string text = mode ? "Switched to Sound Mode" : "Switched to Vibration Mode";
