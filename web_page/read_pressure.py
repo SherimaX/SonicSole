a32a4324e0cab5d72de5ac5567ea511ee6cb090b
hemang4761 <mittalhemang4761@gmail.com>
2024-07-01 11:53:21 -0400

    bug fix

--- web_page/read_pressure.py
+++ web_page/read_pressure.py
@@ -7,6 +7,12 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # Internet, UDP
 sock.bind((UDP_IP, UDP_PORT))
 
 while True:
-    addr = sock.recvfrom(1024)  # Buffer size is 1024 bytes
-    data = int.from_bytes(addr, byteorder='big')
-    print("received message: %s" % data)
+    data, addr = sock.recvfrom(1024)  # Buffer size is 1024 bytes
+    
+    # Convert the received data to an integer (assuming big-endian byte order)
+    try:
+        received_int = int.from_bytes(data, byteorder='big')
+        print(f"Received message: {received_int}")
+
+    except Exception as e:
+        print(f"Error decoding message: {e}")
