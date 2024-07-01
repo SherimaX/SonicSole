dec3523c75153e49fd347a055961710dbae09848
hemang4761 <mittalhemang4761@gmail.com>
2024-07-01 13:19:46 -0400

    readupdpressure bug

--- web_page/app.py
+++ web_page/app.py
@@ -11,12 +11,12 @@ received_data = ""
 
 def read_udp_pressure():
     global received_data
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
-    sock.bind((UDP_IP, UDP_PORT))
+    # sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
+    # sock.bind((UDP_IP, UDP_PORT))
     while True:
-        data, addr = sock.recvfrom(1024)
-        received_data = data.decode('utf-8')
-        print("received message: %s" % data)
+        # data, addr = sock.recvfrom(1024)
+        received_data = 1
+        # print("received message: %s" % data)
 
 def send_udp_data():
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
