b91217a5343c649fa26f0f689eb052248aa4d541
hemang4761 <mittalhemang4761@gmail.com>
2024-07-15 10:24:58 -0400

    naming

--- web_page/app.py
+++ web_page/app.py
@@ -48,7 +48,7 @@ def update_fore_color(pressure):
 def submit():
     global submitted_name
     submitted_name = request.form['name']
-    return redirect(url_for('index'))
+    return redirect(url_for('home'))
 
 def balancing_pressure():
     global totalTime, recording_time, submitted_name
