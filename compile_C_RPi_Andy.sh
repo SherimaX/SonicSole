21c139fd9b4a5fc7d780b3238d3e4f9b260f5327
rajkang <102828595+ragcans@users.noreply.github.com>
2024-07-17 15:38:17 -0400

    changed compile line to include IMU library

--- compile_C_RPi_Andy.sh
+++ compile_C_RPi_Andy.sh
@@ -1,1 +1,2 @@
-g++ -o C_RPi_Andy C_RPi_Andy.cpp SonicSole.cpp -lwiringPi -lpthread -std=c++17
+#g++ -o C_RPi_Andy C_RPi_Andy.cpp SonicSole.cpp -lwiringPi -lpthread -std=c++17
+g++ -o C_RPi_Andy C_RPi_Andy.cpp SonicSole.cpp -I./IMUAPI -L./IMUAPI -lthreespace_api -lwiringPi -lpthread -std=c++17
