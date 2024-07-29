6433dc174606d3e4e595427c6299f73a1156e975
hemang4761 <mittalhemang4761@gmail.com>
2024-07-29 09:40:41 -0400

    2 decimal

--- web_page/app.py
+++ web_page/app.py
@@ -149,11 +149,11 @@ def b_scoreboard():
                     splitted_name = row[0].split("_")
                     if len(splitted_name) > 1:
                         if splitted_name[1] == "0":
-                            data.append({'name': splitted_name[0]+" (Eyes Opened)", 'time': int(float(row[1])*100)*0.01})
+                            data.append({'name': splitted_name[0]+" (Eyes Opened)", 'time':  float(row[1])})
                         else:
-                            data.append({'name': splitted_name[0]+" (Eyes Closed)", 'time': int(float(row[1])*100)*0.01})
+                            data.append({'name': splitted_name[0]+" (Eyes Closed)", 'time': float(row[1])})
                     else:
-                        data.append({'name': row[0], 'time': float(row[1])})
+                        data.append({'name': row[0], 'time':  float(row[1])})
     except FileNotFoundError:
         return "Error: SonicSole2.txt file not found."
     except ValueError:
