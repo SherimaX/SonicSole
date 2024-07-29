8a897199f10bdaed0fabcaf59f74458a3598a5b7
hemang4761 <mittalhemang4761@gmail.com>
2024-07-29 09:29:27 -0400

    decimal

--- web_page/app.py
+++ web_page/app.py
@@ -149,9 +149,9 @@ def b_scoreboard():
                     splitted_name = row[0].split("_")
                     if len(splitted_name) > 1:
                         if splitted_name[1] == "0":
-                            data.append({'name': splitted_name[0]+" (Eyes Opened)", 'time': float(row[1])})
+                            data.append({'name': splitted_name[0]+" (Eyes Opened)", 'time': int(row[1]*100)/0.01})
                         else:
-                            data.append({'name': splitted_name[0]+" (Eyes Closed)", 'time': float(row[1])})
+                            data.append({'name': splitted_name[0]+" (Eyes Closed)", 'time': int(row[1]*100)/0.01})
                     else:
                         data.append({'name': row[0], 'time': float(row[1])})
     except FileNotFoundError:
